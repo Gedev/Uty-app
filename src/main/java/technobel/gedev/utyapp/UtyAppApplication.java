43c0da75a3f1607b308cb46777309b7bd8ed0ba7
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-11-25 10:01:43 +0100

    Initial commit - empty project

---
+++ src/main/java/technobel/gedev/utyapp/UtyAppApplication.java
@@ -0,0 +1,13 @@
+package technobel.gedev.utyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/technobel/gedev/utyapp/UtyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package technobel.gedev.utyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
